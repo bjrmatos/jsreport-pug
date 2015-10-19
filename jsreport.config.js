dc4ebf3dba8dd7707b11e26025de7dc5652ea6c1
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2015-10-19 15:43:09 +0200

    add ace highlighting

--- jsreport.config.js
+++ jsreport.config.js
@@ -2,6 +2,5 @@
 
 module.exports = {
   name: 'jade',
-  main: 'src/jade.js',
-  hasPublicPart: false
+  main: 'src/jade.js'
 };
--- src/jade.js
+++ src/jade.js
@@ -1,10 +1,33 @@
 'use strict';
 
 var jade = require('toner-jade');
+var fs = require('fs');
+var path = require('path');
 
-module.exports = function(reporter) {
+module.exports = function (reporter) {
   reporter.extensionsManager.engines.push({
     name: 'jade',
     pathToEngine: jade
+  });
+
+  //hook to the express extension's ace directory and serve jade highlighting
+  reporter.on("express-configure", function (app) {
+    app.get("/lib/ace/mode-jade.js", function (req, res) {
+      res.send(fs.readFileSync(path.join(__dirname, "../", "public", "js", "ace", "mode-jade.js")));
+    });
+
+    app.get("/lib/ace/snippets/jade.js", function (req, res) {
+      res.send(fs.readFileSync(path.join(__dirname, "../", "public", "js", "ace", "snippets", "jade.js")));
+    });
+  });
+
+  //we need to addFileExtensionResolver after the store provider extension is initialized, but before
+  //every other extension like sample template is processed
+  reporter.initializeListener.insert(0, 'jade', function () {
+    reporter.documentStore.addFileExtensionResolver(function (doc, entitySetName, entityType, propertyType) {
+      if (doc.engine === 'jade' && propertyType.document.engine) {
+        return 'jade';
+      }
+    });
   });
 };
