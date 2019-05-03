9217f5c624af3d8fcb04d5b3a474ca342005fb9b
BJR Matos <bjrmatos@gmail.com>
2019-05-03 09:12:21 -0500

    update for monaco editor

--- studio/main_dev.js
+++ studio/main_dev.js
@@ -1,6 +1,6 @@
-// eslint-disable-next-line import/no-unresolved, import/extensions
+// eslint-disable-next-line import/no-unresolved, import/extensions, import/no-extraneous-dependencies
 import Studio from 'jsreport-studio';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import 'brace/mode/jade';
+import 'monaco-editor/esm/vs/basic-languages/pug/pug.contribution';
 
-Studio.templateEditorModeResolvers.push(template => (template.engine === 'pug' ? 'jade' : null));
+Studio.templateEditorModeResolvers.push(template => (template.engine === 'pug' ? 'pug' : null));
